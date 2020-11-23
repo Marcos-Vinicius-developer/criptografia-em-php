dfb376f797b9f570a7f63e34810f3f92f08f995e
Marcos-Vinicius-developer <74935524+Marcos-Vinicius-developer@users.noreply.github.com>
2020-11-23 19:01:15 -0300

    Adição do árquivo principal.
    Adição do árquivo principal do projeto.

---
+++ Cripto.php
@@ -0,0 +1,64 @@
+<?php
+
+class Cripto {
+    private $senha;
+
+    
+    public function __construct ($senha) {
+    
+        $this->senha = $senha;
+    
+    }
+
+
+    public function criptografar ($tamanhoSalt) {
+        $salt = $this->salt();
+        $hash = md5($this->senha . $salt);
+
+        for ($i = 0; $i < 1500; $i++) {
+            
+            $hash = md5($hash);
+        
+        }
+
+        return ['salt'=>$salt, 'senha'=>$hash];
+        
+    }
+
+    public function descriptografa($senha, $salt, $hash){
+
+        $hashPrograma = md5($senha . $salt);
+
+
+        for ($i = 0; $i < 1500; $i++) {
+            
+            $hashPrograma = md5($hashPrograma);
+        
+        }
+
+        if($hashPrograma == $hash){
+
+            return "true";
+
+        }else{
+
+            return "false";
+
+        }
+
+    }
+
+    public function salt($tamanho = 20) {
+
+        return substr(sha1(mt_rand()), 0, $tamanho);  
+    
+    }
+
+
+}
+
+$cri = new Cripto("123"); // setar uma senha
+
+$usuario = ($cri->criptografar()); // guardar em uma variavel pois será retornado um objeto.
+
+echo $cri->descriptografa("123", $usuario["salt"], $usuario["senha"]); // retornar true se a senha bate, se não, retorna false.
